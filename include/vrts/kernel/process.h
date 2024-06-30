70e705071b54ff8d957a11f270cb1ab86e9f2e5d
Dmitry Novikov <d.novikov@baum.ru>
2024-06-30 14:10:36 +0300

    Project structure updated

---
+++ include/vrts/kernel/process.h
@@ -0,0 +1,12 @@
+#ifndef VRTS_KERNEL_PROCESS_H
+#define VRTS_KERNEL_PROCESS_H
+
+#pragma once
+
+#include <vrts/defs.h>
+
+struct process
+{
+};
+
+#endif // !VRTS_KERNEL_PROCESS_H
---
+++ include/vrts/kernel/thread.h
@@ -0,0 +1,12 @@
+#ifndef VRTS_KERNEL_THREAD_H
+#define VRTS_KERNEL_THREAD_H
+
+#pragma once
+
+#include <vrts/defs.h>
+
+struct thread
+{
+};
+
+#endif // !VRTS_KERNEL_THREAD_H
