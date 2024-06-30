183cfd98c0b764728648ce3209446776a1b0bd67
xerrial <dimass1129@gmail.com>
2024-06-30 20:46:20 +1000

    Introduced vrts_std.h

---
+++ src/vrts_std.h
@@ -0,0 +1,33 @@
+#ifndef VRTS_STD_H
+#define VRTS_STD_H
+
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <limits.h>
+
+// Logical operators ‘and’, ‘or’, ‘not’.
+#include <iso646.h>
+
+#define loop while (true)
+
+#define type_bits(t)   (sizeof(t) * CHAR_BIT)
+
+#define type_signed(t) (! ((t)0 < (t)-1))
+#define type_min(t) ((t) (!type_signed(t) ? (t)0 : ((t)1 << (type_bits(t) - 1))))
+#define type_max(t) ((!type_signed(t) ? (t)-1 : (t) ~ (~(t)0 << (type_bits(t) - 1))))
+
+// Applicable to static arrays only.
+#define lengthof(x) (sizeof(x) / sizeof(*(x)))
+
+#if !defined(min)
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#endif
+
+#if !defined(max)
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#endif
+
+#endif VRTS_STD_H
